903c2572db3dfe7a7e545c7284d9bbd0a4989e52
Waldomiro Barbosa Romão <devwaldomiroromao@gmail.com>
2019-11-23 00:35:55 -0300

    [ADD] Projeto & Movimentação
    Primeira atualização. Adição do projeto Unity, junto com menu inicial e movimentação do personagem.

---
+++ Snowperson/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+    public string nextScene;
+    public static bool isPaused;
+    public GameObject pauseMenuUI;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(isPaused){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+    
+    public void changeScene () {
+        SceneManager.LoadScene(nextScene);
+        //if(nextScene == "FASE01") FindObjectOfType<AudioManager>().Play("Starting");
+        //else 
+        FindObjectOfType<AudioManager>().Play("buttonPress");
+    }
+
+    public void quitApplication () {
+        Application.Quit();
+        FindObjectOfType<AudioManager>().Play("buttonPress");
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+        //FindObjectOfType<AudioManager>().Play("ButtonPress");
+    }
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+        //FindObjectOfType<AudioManager>().Play("ButtonPress");
+    }
+}
+
---
+++ Snowperson/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public float speed;
+	private bool facingRight;
+	private bool facingUp;
+
+
+	private Rigidbody2D rb;
+	private SpriteRenderer sprite;
+	public Vector2 movement;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	rb = GetComponent<Rigidbody2D>();
+    	sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   		movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y =Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        Flip();
+    }
+
+    void Flip() 
+    {
+    	if (movement.x < 0)
+    	{
+    		facingRight = false;
+    		transform.localRotation = Quaternion.Euler(0,90,0);
+    	}
+    	else if (movement.x > 0)
+    	{
+    		facingRight = true;
+    		transform.localRotation = Quaternion.Euler(0,0,0);
+    	}
+    	
+    	if(movement.y < 0)
+    	{
+    		facingUp = false;
+    		transform.localRotation = Quaternion.Euler(0,90,0);
+    	}
+    	else if (movement.y > 0)
+    	{
+    		facingUp = true;
+    		transform.localRotation = Quaternion.Euler(0,270,0);
+    	}
+    }
+
+
+}
